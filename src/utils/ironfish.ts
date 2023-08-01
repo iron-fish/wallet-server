216bf0df9f8e3fb589de1e3b626ac04f1e0e8eb7
Daniel Cortes <danielgcortes@gmail.com>
2023-08-01 10:57:35 -0600

    Cache sdk

--- src/utils/ironfish.ts
+++ src/utils/ironfish.ts
@@ -1,6 +1,7 @@
 import { IronfishSdk, RpcClient } from "@ironfish/sdk";
 import { logger } from "./logger";
 
+let sdk: IronfishSdk | null = null;
 let rpcClient: RpcClient | null = null;
 
 export async function getIronFishClient(): Promise<RpcClient | null> {
@@ -8,11 +9,16 @@ export async function getIronFishClient(): Promise<RpcClient | null> {
     return rpcClient;
   }
 
-  const sdk = await IronfishSdk.init();
+  if (!sdk) {
+    sdk = await IronfishSdk.init();
+  }
 
   try {
     rpcClient = await sdk.connectRpc(false, true);
   } catch (err) {
+    // Todo:
+    // - Add retry logic
+    // - Add error handling
     logger.error("Error connecting to IronFish RPC", err);
   }
 
