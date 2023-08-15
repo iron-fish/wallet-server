fc4d921901df2266460e7d3a02cc62d8ed155a97
Joe <joe@iflabs.network>
2023-08-15 12:32:16 -0700

    adds merkle tree example using ironfish merkle tree implementation

---
+++ example/src/merkle/merkle.ts
@@ -0,0 +1,25 @@
+import { NoteHasher } from "@ironfish/sdk/build/src/merkletree/hasher";
+import { MerkleTree } from "@ironfish/sdk/build/src/merkletree/merkletree";
+import { BUFFER_ENCODING } from "@ironfish/sdk/build/src/storage";
+import { createDB } from "@ironfish/sdk/build/src/storage/utils";
+import { LeafEncoding } from "@ironfish/sdk/build/src/merkletree/database/leaves";
+import { NodeEncoding } from "@ironfish/sdk/build/src/merkletree/database/nodes";
+import { NoteEncrypted } from "@ironfish/sdk/build/src/primitives/noteEncrypted";
+
+const db = createDB({ location: "./testdb" });
+db.open();
+
+const notesTree = new MerkleTree({
+  hasher: new NoteHasher(),
+  leafIndexKeyEncoding: BUFFER_ENCODING,
+  leafEncoding: new LeafEncoding(),
+  nodeEncoding: new NodeEncoding(),
+  db,
+  name: "n",
+  depth: 32,
+  defaultValue: Buffer.alloc(32),
+});
+
+export const addNotesToMerkleTree = async (notes: NoteEncrypted[]) => {
+  return notesTree.addBatch(notes);
+};
